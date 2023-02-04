7ff229ae4c66047459ad79b1d0c1f7112fdd0936
doqedev <tjvendingllc@gmail.com>
2023-02-03 23:01:45 -0700

    idk

--- index.js
+++ index.js
@@ -1,51 +1,58 @@
-const { getUserInfo } = require("@replit/repl-auth")
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// Call all packages
+
 const express = require('express');
+const { getUserInfo } = require('@replit/repl-auth')
+const passport = require('passport')
+const MagicStrat = require('passport-magic').Strategy;
 const { Server } = require("socket.io");
-const { Magic } = require('@magic-sdk/admin');
-const magic = new Magic(process.env.magicSecret);
+const http = require('http')
+// const { Magic } = require('@magic-sdk/admin');
+// const magic = new Magic(process.env.magicSecret); -- coming soon!!
 
 const app = express();
 app.set('view engine', 'ejs')
 app.set('views', __dirname + "/views")
 app.use(express.static('public'))
-const http = require('http');
+
+app.post('/login', passport.authenticate('magic'));
 const server = http.createServer(app);
 const io = new Server(server);
 
 io.on('connection', (socket) => {
   const user = getUserInfo(socket.request)
-  if(!user) return socket.disconnect();
-  let name = `${socket.id.slice(0,4)}`
-  
+  if (!user) return socket.disconnect();
+  let name = `${socket.id.slice(0, 4)}`
+
   socket.on('send', (msg) => {
-    if(socket.ghost) return;
-    if(msg == ""){
+    if (socket.ghost) return;
+    if (msg == "") {
       socket.ghost = true
-    }else{
+    } else {
       io.sockets.emit("message", name, msg, socket.id)
     }
     console.log(`[${user.name}]: ${msg}`)
   })
-  
+
   console.log(`${name} has connected.`);
-  
+
 });
 
 app.get('/', (req, res) => {
   const user = getUserInfo(req)
-  if(!user) return res.render('magicLogin');
-  res.render('index', {user: user.name})
+  if (!user) return res.render('login');
+  res.render('index', { user: user.name })
 });
 
-app.get('/callback', (req,res) => {
-  res.render('magicCallback')
-})
-
-app.get('/logout', (req,res) => {
-  res.render('magicLogout')
-})
-
 server.listen(3000, () => {
-  console.log(`https://${process.env.REPL_ID}.id.repl.co/`)
+  if (process.env.REPL_ID) {
+    console.log(`https://${process.env.REPL_ID}.id.repl.co/`)
+  }
+  else {
+    console.log(`http://localhost:3000`)
+  }
   console.log('server started');
 });
